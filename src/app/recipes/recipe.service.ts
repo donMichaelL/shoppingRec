2157f68dc8e42d654df1a5cf6b171dcc4d958380
Michael <michael.loukeris@icloud.com>
2018-05-07 13:06:37 +0300

    Add ingredients table to recipe and display

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,21 @@
 import { Recipe } from '../recipes/recipe.model'
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Hamburger', 'This is a new burger', 'http://2wk128489wjq47m3kwxwe9hh.wpengine.netdna-cdn.com/wp-content/uploads/2015/03/burgers-bacon-guacamole-burger-main.jpg'),
-        new Recipe('Cheeseburger', 'This is a new burger', 'http://2wk128489wjq47m3kwxwe9hh.wpengine.netdna-cdn.com/wp-content/uploads/2015/03/burgers-bacon-guacamole-burger-main.jpg')
+        new Recipe(
+            'Hamburger', 
+            'This is a new burger', 
+            'http://2wk128489wjq47m3kwxwe9hh.wpengine.netdna-cdn.com/wp-content/uploads/2015/03/burgers-bacon-guacamole-burger-main.jpg',
+            [new Ingredient('meat', 1), new Ingredient('cucumber',2)]),
+        new Recipe('Cheeseburger', 
+            'This is a new burger', 
+            'http://2wk128489wjq47m3kwxwe9hh.wpengine.netdna-cdn.com/wp-content/uploads/2015/03/burgers-bacon-guacamole-burger-main.jpg',
+            [new Ingredient('meat', 2), new Ingredient('nothing', 3)])
       ];
     
     getRecipes() {
