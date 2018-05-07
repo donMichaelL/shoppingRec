8ae8ca800c552f6359e2cc7f82661dde6fff3820
Michael <michael.loukeris@icloud.com>
2018-05-07 12:42:25 +0300

    Add cross component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
 import { Recipe } from '../recipes/recipe.model'
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('Hamburger', 'This is a new burger', 'http://2wk128489wjq47m3kwxwe9hh.wpengine.netdna-cdn.com/wp-content/uploads/2015/03/burgers-bacon-guacamole-burger-main.jpg'),
         new Recipe('Cheeseburger', 'This is a new burger', 'http://2wk128489wjq47m3kwxwe9hh.wpengine.netdna-cdn.com/wp-content/uploads/2015/03/burgers-bacon-guacamole-burger-main.jpg')
